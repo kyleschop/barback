a0bfe1534308191a2edfca8c43939b76070559bd
nickrosales <116924957+nickrosales@users.noreply.github.com>
2022-12-19 21:12:31 -0700

    added header, linked tailwind and google font, added searchbar to index.html, added google font to style.css, added image to assets folder

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//"https://api.edamam.com/api/recipes/v2?type=public&q="+ userInput.value +"&app_id=35d6513c&app_key=1522b093a826cd1756220f536462ec8e&dishType=Drinks"
+
+//var userInput = document.querySelector("#default-search");
+//var 
+
+
+
